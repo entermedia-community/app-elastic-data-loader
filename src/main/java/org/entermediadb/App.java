b22c0d476528bd73a704150eaa87fb7bea82de4c
Christopher Burkey <cburkey@entermediadb.org>
2021-01-04 15:57:49 -0600

    Added import

--- src/main/java/org/entermediadb/App.java
+++ src/main/java/org/entermediadb/App.java
@@ -1,27 +1,35 @@
 package org.entermediadb;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.core.AcknowledgedResponse;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
-import org.elasticsearch.client.indices.GetMappingsRequest;
-import org.elasticsearch.client.indices.GetMappingsResponse;
-import org.elasticsearch.client.indices.PutMappingRequest;
-import org.elasticsearch.cluster.metadata.MappingMetadata;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.client.security.RefreshPolicy;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
 
 /**
  * Hello world!
@@ -29,24 +37,23 @@ import org.elasticsearch.common.xcontent.XContentFactory;
  */
 public class App 
 {
+	private static final String GITHOME = "/home/shanti/git/app-elastic-data-loader";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Connecting" );
         
         try
         {
-	    		RestHighLevelClient client = new RestHighLevelClient(
-	    		        RestClient.builder(
-	    		                new HttpHost("154.127.54.122", 9270, "http")
-	    		                //,    new HttpHost("localhost", 9201, "http"))
-	    		             )
-	    		 );
-	    		//setMapping(client);
-	    		//loadData(client);
+        	App app = new App();
+        	RestHighLevelClient client = app.connect();
+        	//app.setMapping
+	    	//app.setMapping(client,"assets_catalog", "asset");
+        	//app.createIndex(client,"assets_catalog4", "asset");
+	    	app.loadData(client,"assets_catalog4", "asset");
 	    		
 	    		// getMapping(client, "twitter");
-	    		insertDocument(client, "posts", "2");
-	    		getDocument(client);
+	    		//insertDocument(client, "posts", "2");
+	    		//getDocument(client);
 	    		client.close();
 		}
 		catch (Throwable ex)
@@ -55,7 +62,70 @@ public class App
 		}
     }
     
-    public static void insertDocument(RestHighLevelClient client, String index, String id) {
+    private RestHighLevelClient connect()
+	{
+    	RestHighLevelClient client = new RestHighLevelClient(
+		        RestClient.builder(
+		                new HttpHost("154.127.54.122", 9270, "http")
+		                //,    new HttpHost("localhost", 9201, "http"))
+		             )
+		 );
+		return client;
+	}
+
+
+    public void createIndex(RestHighLevelClient client, String catalogid, String module) throws Exception
+    {
+			CreateIndexRequest request = new CreateIndexRequest(catalogid + "_" + module);
+//			File file = new File(GITHOME + "/data/elasticindex.yaml");
+//			FileInputStream stream = new FileInputStream(file);
+//			StreamInput input = new InputStreamStreamInput(stream);
+//			request.settings(Settings.readSettingsFromStream(input) 				);
+	    	String settings = readfile("/home/shanti/git/app-elastic-data-loader/data/elasticindex.yaml");
+			request.settings(settings,XContentType.YAML);
+			//stream.close();
+				//Builder settingsBuilder = Settings.builder().loadFromStream(yaml.getName(), in);
+//
+//				for (Iterator iterator = getLocalNode().getProperties().keySet().iterator(); iterator.hasNext();)
+//				{
+//					String key = (String) iterator.next();
+//					if (key.startsWith("index.")) //Legacy
+//					{
+//						String val = getLocalNode().getSetting(key);
+//						settingsBuilder.put(key, val);
+//					}
+//				}
+//
+//				CreateIndexResponse newindexres = admin.indices().prepareCreate(index).setSettings(settingsBuilder).execute().actionGet();
+
+    			
+//    			XContentBuilder builder = XContentFactory.jsonBuilder();
+//    			builder.startObject();
+//    			{
+//    			    builder.startObject("properties");
+//    			    {
+//    			        builder.startObject("message");
+//    			        {
+//    			            builder.field("type", "text");
+//    			        }
+//    			        builder.endObject();
+//    			    }
+//    			    builder.endObject();
+//    			}
+//    			builder.endObject();
+//    			request.mapping(builder);
+
+    	    	String mapping = readfile("/home/shanti/git/app-elastic-data-loader/data/"+ module + "-mapping.json");
+    	    	request.mapping(mapping, XContentType.JSON); 
+
+    			//request.alias(new Alias("twitter_alias").filter(QueryBuilders.termQuery("user", "kimchy")));
+    			CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
+    	        System.out.println( "complete " + createIndexResponse.toString() );
+    			
+    			//client.search(searchRequest, options);
+    }
+    
+	public static void insertDocument(RestHighLevelClient client, String index, String id) {
     	Map<String, Object> food = new HashMap<>();
     	food.put("food", "kimchy");
     	food.put("postDate", new Date());
@@ -89,7 +159,7 @@ public class App
     }
     
     
-    public static void getMapping(RestHighLevelClient client, String index) 
+/*    public static void getMapping(RestHighLevelClient client, String index) 
     {
     	try 
     	{
@@ -106,35 +176,55 @@ public class App
     		ex.printStackTrace();
     	}
     }
-    
-    
-    
-    public static void setMapping(RestHighLevelClient client) {
-    	PutMappingRequest request = new PutMappingRequest("twitter"); 
+  */
+    public String readfile(String path) throws Exception
+    {
+    	StringBuffer output = new StringBuffer();
     	
-    	try {
-    	XContentBuilder builder = XContentFactory.jsonBuilder();
-    	builder.startObject();
-    	{
-    	    builder.startObject("properties");
-    	    {
-    	        builder.startObject("message");
-    	        {
-    	            builder.field("type", "text");
-    	        }
-    	        builder.endObject();
-    	    }
-    	    builder.endObject();
-    	}
-    	builder.endObject();
-    	request.source(builder); 
-    	org.elasticsearch.action.support.master.AcknowledgedResponse putMappingResponse = client.indices().putMapping(request, RequestOptions.DEFAULT);
-    	}
-    	catch (Throwable ex)
-		{
-			ex.printStackTrace();
-		}
+    	 try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                 new FileInputStream(path), StandardCharsets.UTF_8));) {
+
+             String line;
+             
+             while ((line = br.readLine()) != null) {
+                 
+            	 output.append(line);
+            	 output.append("\n");
+             }
+         }
+
+         return output.toString();
     }
+
+    /*
+    public void setMapping(RestHighLevelClient client, String catalogid, String module) throws Exception 
+    {
+        System.out.println( "maping mapping "+ catalogid + " " + module );
+
+    	String mapping = readMappingfile(GITHOME + "/data/"+ module + "-mapping.json");
+    	
+    	PutMappingRequest request = new PutMappingRequest(catalogid + "_" + module); 
+    	
+//    	XContentBuilder builder = XContentFactory.jsonBuilder();
+//    	builder.startObject();
+//    	{
+//    	    builder.startObject("properties");
+//    	    {
+//    	        builder.startObject("message");
+//    	        {
+//    	            builder.field("type", "text");
+//    	        }
+//    	        builder.endObject();
+//    	    }
+//    	    builder.endObject();
+//    	}
+//    	builder.endObject();
+    	
+    	request.source(mapping, XContentType.JSON); 
+    	
+    	org.elasticsearch.action.support.master.AcknowledgedResponse putMappingResponse = client.indices().putMapping(request, RequestOptions.DEFAULT);
+    }
+    */
     /*
     public void setMapping(RestHighLevelClient client, String indexname, String tablename) throws Exception
     {
@@ -156,27 +246,17 @@ public class App
     }
     */
     
-    /*
-    public void loadData(RestHighLevelClient client)
+    
+    public void loadData(RestHighLevelClient client, String catalogid, String module) throws Exception
     {
-    	//public void importJson(Data site, MediaArchive mediaarchive, String searchtype, Page upload, String tempindex) throws Exception{
+    		FileInputStream inputfile = new FileInputStream(new File( GITHOME + "/data/small.zip") );
 
+    		try
+    		{
+    			ZipInputStream unzip = new ZipInputStream(inputfile);
+    			//ZipEntry entry = unzip.getNextEntry();
 
-    		Searcher searcher = mediaarchive.getSearcher(searchtype);
-    		if(searcher instanceof ElasticListSearcher){
-    			return;
-    		}
-    		ElasticNodeManager manager = (ElasticNodeManager)mediaarchive.getNodeManager();
-    		
-    		BulkProcessor processor = manager.getBulkProcessor();
-    		
-    		try{
-
-
-    			ZipInputStream unzip = new ZipInputStream(upload.getInputStream());
-    			ZipEntry entry = unzip.getNextEntry();
-
-
+   		        BulkRequest br = new BulkRequest();
 
     			MappingJsonFactory f = new MappingJsonFactory();
     			JsonParser jp = f.createParser(new InputStreamReader(unzip, "UTF-8"));
@@ -184,104 +264,62 @@ public class App
     			JsonToken current;
 
     			current = jp.nextToken();
-    			if (current != JsonToken.START_OBJECT) {
+    			if (current != JsonToken.START_OBJECT)
+    			{
     				System.out.println("Error: root should be object: quiting.");
     				return;
     			}
 
-    			while (jp.nextToken() != JsonToken.END_OBJECT) {
+    			while (jp.nextToken() != JsonToken.END_OBJECT) 
+    			{
     				String fieldName = jp.getCurrentName();
     				// move from field name to field value
     				current = jp.nextToken();
-    				if (fieldName.equals(searchtype)) {
-    					if (current == JsonToken.START_ARRAY) {
-    						// For each of the records in the array
-    						while (jp.nextToken() != JsonToken.END_ARRAY) {
-    							// read the record into a tree model,
-    							// this moves the parsing position to the end of it
-    							JsonNode node = jp.readValueAsTree();
-    							IndexRequest req = Requests.indexRequest(tempindex).type(searchtype);
-    							String json  = node.toString();
-    							
-    							req.source(json);
-    							JsonNode id = node.get("id");
-    							if( id == null)
-    							{
-    								log.info("No ID found " + searchtype + " node:" + node);
-    							}
-    							else
-    							{
-    								req.id(id.asText());
-    							}	
-    							processor.add(req);
-
-    						}
-    					} else {
-    						System.out.println("Error: records should be an array: skipping.");
-    						jp.skipChildren();
-    					}
-    				} else {
-    					System.out.println("Unprocessed property: " + fieldName);
-    					jp.skipChildren();
-    				}
+					if (current == JsonToken.START_ARRAY) {
+						// For each of the records in the array
+						while (jp.nextToken() != JsonToken.END_ARRAY) {
+							// read the record into a tree model,
+							// this moves the parsing position to the end of it
+							JsonNode node = jp.readValueAsTree();
+							IndexRequest req = Requests.indexRequest(catalogid + "_" + module);
+							String json  = node.toString();
+		    		        //client.bulk(br, RequestOptions.DEFAULT);
+							req.source(json);
+							
+							JsonNode id = node.get("id");
+							if( id == null)
+							{
+								info("No ID found " + module + " node:" + node);
+							}
+							else
+							{
+								req.id(id.asText());
+							}	
+							br.add(req);
+						}
+					} else {
+						System.out.println("Error: records should be an array: skipping.");
+						jp.skipChildren();
+					}
     			}
+//				} else {
+//					System.out.println("Unprocessed property: " + fieldName);
+//					jp.skipChildren();
+//				}
     		}
-    		finally{
+    		finally
+    		{
 
-    			manager.flushBulk();
-
-    				//This is in memory only flush
-    				//RefreshResponse actionGet = getClient().admin().indices().prepareRefresh(catid).execute().actionGet();
-
-
-
-    		
-
-
-
+    			inputfile.close();
     	//	}
 
     	}
 
     }
-    */
-    
-    
-    public void createIndex(RestHighLevelClient client)
-    {
-    		try
-    		{
-    			CreateIndexRequest request = new CreateIndexRequest("assets_catalog_asset");
-    			request.settings(Settings.builder() 
-    				    .put("index.number_of_shards", 3)
-    				    .put("index.number_of_replicas", 2)
-    				);
-    			
-    			XContentBuilder builder = XContentFactory.jsonBuilder();
-    			builder.startObject();
-    			{
-    			    builder.startObject("properties");
-    			    {
-    			        builder.startObject("message");
-    			        {
-    			            builder.field("type", "text");
-    			        }
-    			        builder.endObject();
-    			    }
-    			    builder.endObject();
-    			}
-    			builder.endObject();
-    			request.mapping(builder);
-    			
-    			//request.alias(new Alias("twitter_alias").filter(QueryBuilders.termQuery("user", "kimchy")));
-    			CreateIndexResponse createIndexResponse = client.indices().create(request, RequestOptions.DEFAULT);
-    			
-    			//client.search(searchRequest, options);
-    			
-    		}
-    		catch (Throwable ex)
-    		{
-    			ex.printStackTrace();
-    		}
-    }
+
+	private void info(String inString)
+	{
+		// TODO Auto-generated method stub
+		System.out.println(inString);
+	}
 }
